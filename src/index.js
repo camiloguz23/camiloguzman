73a66bcc5536edeb86e16992508c1f805593fd59
Camilo Guzman <jeysonkmguzman@gmail.com>
2022-01-12 10:54:05 -0500

    sw

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import ReactDom from 'react-dom'
 import React from 'react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { CVpages} from './CVpages.js';
 import './css/main.css'
 
 const html = document.getElementById("html")
 
 ReactDom.render(<CVpages/> , html)
+
+serviceWorkerRegistration.register();
